f8c219a0c251437ecafdb2dd214ed6d5d3bbec37
RenatoDev <renatoyaranga1997@gmail.com>
2025-12-03 18:16:13 -0500

    feat: Implementar el panel de control inicial del profesor con páginas de diseño, inicio, calificaciones y asistencia.

---
+++ app/(profesor)/profesor/asistencia/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function AsistenciaPage() {
+  return (
+    <main className="w-full h-full min-h-screen relative flex items-center justify-center p-4 sm:p-6 lg:p-8">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      >
+        <source src="/Fondo-Admin.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full bg-white/50 -z-10"></div>
+    <div>Asistencia Page</div>
+    </main>
+  )
+}
---
+++ app/(profesor)/profesor/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import "../../ui/globals.css";
+import SidebarProfesor from "@/app/components/view/sideBarProfesor";
+
+export const metadata: Metadata = {
+  title: "Area Admin",
+  description: `Area administrativa del Colegio Jesus Angels`,
+  icons: {
+    icon: "/Logo.svg",
+  }
+};
+
+export default function AdminLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <html lang="es">
+      <body className="flex bg-gray-100 h-screen overflow-hidden m-0 p-0">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="fixed top-0 left-0 w-full h-full object-cover -z-10"
+        >
+          <source src="/Fondo-Admin.mp4" type="video/mp4" />
+        </video>
+        <div className="fixed top-0 left-0 w-full h-full bg-white/50 -z-10"></div>
+
+        <SidebarProfesor />
+        <main className="flex-1 overflow-y-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
